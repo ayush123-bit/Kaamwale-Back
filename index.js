ac8575fc108830705d381b3311a93bfaca8b38a5
Ayush Rai <145470522+ayush123-bit@users.noreply.github.com>
2025-07-28 16:53:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://scintillating-kelpie-de0c3d.netlify.app/',
+    origin:'https://scintillating-kelpie-de0c3d.netlify.app',
     credentials:true
 }
 
